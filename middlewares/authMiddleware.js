7bc3f7ff894a32e18ec2d815bb0e358da969c4ca
Naveen Singh <naveensingh7604@gmail.com>
2023-11-29 13:56:44 +0530

    some minor hanges

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,29 +6,29 @@ export const auth = async (req, res, next) => {
   // its working in development
   next();
 
-  try {
-    const token = req.cookies.token;
-    if (!token) {
-      res.status(401).send({
-        success: false,
-        message: "Not Authorised!, Please login",
-      });
-    }
-    // verify token
-    const verified = jwt.verify(token, process.env.JWT_Key);
-    // get user id from token
-    const user = await adminModel.findById(verified.id).select("-password");
-    if (!user) {
-      res.status(401).send({
-        success: false,
-        message: "User Not Found",
-      });
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log(error);
-    res.status(401);
-    // throw new Error("Not Authorised!!!!!, Please login");
-  }
+  // try {
+  //   const token = req.cookies.token;
+  //   if (!token) {
+  //     res.status(401).send({
+  //       success: false,
+  //       message: "Not Authorised!, Please login",
+  //     });
+  //   }
+  //   // verify token
+  //   const verified = jwt.verify(token, process.env.JWT_Key);
+  //   // get user id from token
+  //   const user = await adminModel.findById(verified.id).select("-password");
+  //   if (!user) {
+  //     res.status(401).send({
+  //       success: false,
+  //       message: "User Not Found",
+  //     });
+  //   }
+  //   req.user = user;
+  //   next();
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(401);
+  //   // throw new Error("Not Authorised!!!!!, Please login");
+  // }
 };
